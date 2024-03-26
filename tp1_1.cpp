5baa59408045859904a0dd1708d8f255361c42e9
Nicholas1000200 <nico-laz@hotmail.com>
2024-03-26 15:58:29 -0300

    tercer commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,5 +1,18 @@
-
+#include <stdio.h>
 
 int main(){
     printf("Hola mundo");
+    int var1 = 6;
+    int *pvar1;
+    pvar1 = &var1;
+    
+
+
+    printf("El contenido del puntero es: %d",*pvar1);
+    printf("La direccion de memoria almacenada en el puntero es: %d",pvar1);
+    printf("La direccion de memoria de la variable es: %d",&var1);
+    printf("La direccion de memoria del puntero es: %d",&pvar1);
+    printf("El contenido del puntero es: %d",*pvar1);
+
+    printf("El tamaño de la variable es %d",sizeof(var1));
 }
